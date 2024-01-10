097e85aa4022ec5ec799f1314d486d03b9fab4c9
vietthuan98 <44547968+vietthuan98@users.noreply.github.com>
2024-01-10 16:56:33 +0700

    fix eslint (#1)
    * fix eslint
    
    * fix eslint
    
    ---------
    
    Co-authored-by: lvt <thuanlv@vmogroup.com>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from './app.js';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server running at http://127.0.0.1:3000/");
+	console.log('Server running at http://127.0.0.1:3000/');
 });
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { fileRouter } from './modules/file/router.js';
 import { streamVideoRouter } from './modules/stream-video/router.js';
  
-const router = express.Router()
+const router = express.Router();
 
 // middleware that is specific to this router
 fileRouter(router);
