e1d012335a5fa134f27105a78616e1f577c892c6
lvt <thuanlv@vmogroup.com>
2024-01-10 16:27:40 +0700

    add eslint cli

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 // Load the necessary modules and define a port
-import express from "express";
+import express from 'express';
 import router from './router.js';
 
 const app = express();
 
-app.use(express.static('src/public'))
+app.use(express.static('src/public'));
 
-app.get("/", (_, res) => {
-  res.status(200).send('/index.html');
+app.get('/', (_, res) => {
+	res.status(200).send('/index.html');
 });
 
-app.get("/ping", (_, res) => {
-  res.status(200).send(`pong`);
+app.get('/ping', (_, res) => {
+	res.status(200).send('pong');
 });
 
 app.use('/api', router);
